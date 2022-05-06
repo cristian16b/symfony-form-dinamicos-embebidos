d93cbc5370a74286aa9108d39fdb22ab7d916bd6
cristian16b <cristian.budzicz@gmail.com>
2022-05-06 20:45:31 -0300

    first commit

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+// src/Controller/TaskController.php
+namespace App\Controller;
+
+use App\Entity\Tag;
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TaskController extends AbstractController
+{
+        /**
+     * @Route("/blog", name="blog_list")
+     */
+    public function new(Request $request): Response
+    {
+        $task = new Task();
+
+        // dummy code - add some example tags to the task
+        // (otherwise, the template will render an empty list of tags)
+        // $tag1 = new Tag();
+        // $tag1->setName('tag1');
+        // $task->getTags()->add($tag1);
+        // $tag2 = new Tag();
+        // $tag2->setName('tag2');
+        // $task->getTags()->add($tag2);
+        // end dummy code
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... do your form processing, like saving the Task and Tag entities
+            dump($form->getData());die;
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
